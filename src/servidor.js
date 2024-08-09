14b89242bf40e52a937dd1c9618137a16ae69d57
Erick Yoseth Noguera <155571418+ErickNoguera@users.noreply.github.com>
2024-08-09 17:55:53 -0400

    cambios

---
+++ src/servidor.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import Router from "axios"
+import 'dotenv/config'
+
+
+
+const express = require('express')
+const app = expres (() => {
+    return console.log("escuchando en el puero 5147")
+})
+
+use.Router(listen)
